8efd88d9df03f14ff894da21446b36cb6642f4d8
vincentgordon97 <66184004+vincentgordon97@users.noreply.github.com>
2023-02-27 17:47:00 -0500

    Added choropleth map

--- Plotly/static/app.js
+++ Plotly/static/app.js
@@ -80,6 +80,7 @@ function buildBarChart(countryCode) {
             type: "bar"
         }
         
+        // Set leayout of bar chart
         let layout = {
             title: "Total Deaths by Tesla Model"
         }
@@ -88,9 +89,71 @@ function buildBarChart(countryCode) {
         Plotly.newPlot("bar", barTrace, layout);
     });
 }
-     
-    // Initialize onChange function
-    function optionChanged(newData) {
-        buildBarChart(newData);
+
+
+    // Need to find state code
+d3.json(casetablePath).then(function(data) {
+    var result = [];
+    data.reduce(function(res, value) {
+        if (!res[value.state_id]) {
+            res[value.state_id] = {state_id: value.state_id, deathtotal: 0};
+            result.push(res[value.state_id])
+        }
+        res[value.state_id].deathtotal += value.deathtotal;
+        return res;
+    }, {});
+
+    cleanedArray = [];
+    for (let i = 0; i < result.length; i++) {
+        if (result[i].state_id != null) {
+            cleanedArray.push(result[i])
+        }
+    }   
+
+    deathCount = cleanedArray.map(item => item.deathtotal);
+
+});
+
+// Use for the choropleth mapbox
+d3.json(statecoordsPath).then(function (data) {
+
+    for (let i = 0; i < data.length; i++) {
+        row = data[i];
+        
+        // Get the unique state names
+        stateName = data.map(item => item.state_name)       
     }
+    
+
+ // Remove whitespaces from array
+    stateName = stateName.map(item => item.trim())
+    
+
+    let trace = {
+        type: "choropleth",
+        locationmode: 'USA-states',
+        locations: stateName,
+        z: deathCount,
+    };
+
+    let layout = {
+        titel: "Death Total per US State",
+        geo: {scope: "usa",
+        showland: true},
+        width: 600,
+        height: 500
+    };
+
+    let Trace = [trace]
+
+    let config = {mapboxAccessToken: "pk.eyJ1IjoiZHVtbXlnb3Jkb245NyIsImEiOiJjbGVuY3ZnMm0xZHhnM3hxdnhmenMzejY4In0.34y53mYNsQLEGsgAlkKrLQ"};
+
+    Plotly.newPlot("map", Trace, layout, config)
+})
+
+// Initialize onChange function
+function optionChanged(newData) {
+    buildBarChart(newData);
+};
+
 init();
